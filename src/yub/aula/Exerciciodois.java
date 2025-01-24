91f9f338109db6777bcebb1ee2710b216068bdcd
Livson Lima <livsonlima@gmail.com>
2025-01-24 00:45:02 -0300

    Adicionado informação de aula3

--- src/yub/aula/Exerciciodois.java
+++ src/yub/aula/Exerciciodois.java
@@ -7,11 +7,17 @@ public class Exerciciodois {
         /* tipos primitivos*/
 
         byte idade = 50;
-        int numeroCasa = 1819;   // MÉTODO MUITO UTILIZADO PARA IMPRIMIR NÚMEROS
+        int numeroCasa = 1819; // MÉTODO MUITO UTILIZADO PARA IMPRIMIR NÚMEROS
+        int cep = 25957005;   // SE COMEÇAR COM ZERO TALVEZ DEVERÁ SER OUTRO TIPO
         short numeroCadastro = 1345;
+        short ano = 2025;
+        long CPF = 86321285400L; // SE COMEÇAR COM ZERO TALVEZ DEVERÁ SER OUTRO TIPO // DEVERÁ CONSTAR O "L" NO FINAL DO VALOR
+        
 
         float preco =34.96f; 
+        float pi = 3.14F;  // DEVERÁ CONSTAR O "F" NO FINAL DO VALOR
         double total = 456.9867;
+        double salario = 1275.33;
         boolean itemVisivel = false; // true(verdadeiro) false (false); -                              MÉTODO UTILIZADO PARA COMPARAÇÕES
        
     /*  **Classes Wrapper* **/  
